f2bb41124a9d932ce3b49277d58a75473b8934a0
bojana-petroska <bojana.petroska@yahoo.com>
2024-05-22 18:17:43 +0200

    11_day_end

---
+++ 11_day_afternoon.js
@@ -0,0 +1,90 @@
+// Function Scope:
+// const globalVar = 'bo';
+
+// function localScope() {
+//     const localVar = 20;
+//     console.log(globalVar);
+//     console.log(localVar);
+// }
+
+// console.log(localScope());
+//console.log(localVar);
+
+// Loops and Arrays:
+// Task 2: Sum of Array Elements:
+
+const sumArray = function(numbers) {
+    if(Array.isArray(numbers) && numbers.every(number => typeof number === 'number')) {
+        let sum = 0;
+        numbers.forEach(number => {
+            sum += number;
+        });
+        return sum;
+    } else return 'Error! Put only numbers!'
+}
+console.log(sumArray([4, 6, 8, 9]));
+console.log(sumArray([4, 6, 8, 9, 10, 12]));
+console.log(sumArray([4, 6, 8, 9, 10, 'bo']));
+
+// Task 3: Reverse Array Elements:
+
+const reverseArray = arr => {
+    if(arr.every(elem => typeof elem === 'string')) {
+        arr.reverse();
+    };
+    return arr;
+}
+
+const reverseArray1 = arr => {
+    if(arr.every(elem => typeof elem === 'string')) {
+        let arrReverse = [];
+        for (let i = arr.length-1; i >= 0; i--) {
+            arrReverse.push(arr[i])
+        }
+        return arrReverse;
+    };
+}
+
+console.log(reverseArray(['bo', 'petroska']));
+console.log(reverseArray1(['bo', 'petroska']));
+
+// Objects:
+// Task 4: Student Information:
+
+const student = {
+    name: 'bo',
+    age: 37,
+    grade: 5
+}
+
+function displayInfo(arg) {
+    console.log(`Her name is ${student.name}.`);
+    console.log(`She is ${student.age} years old.`);
+    console.log(`And her grade is ${student.grade}.`)
+}
+
+displayInfo({name: "bo", age: 37, grade: 5});
+
+// Task 5: Shopping Cart:
+const cart = {
+    items: [],
+    total: 0,
+    addItem(item) {
+        
+
+    }
+};
+
+// Bonus Task: Task 6 - Finding Maximum Number in an Array:
+
+function findMax(arr = []) {
+    if(!Array.isArray(arr)) {
+        throw new Error('Buhuuu, only arrays are accepted!');
+    }
+    if(!arr.every(num => typeof num === 'number')) {
+        throw new Error('Buhuuu, only numbers are accepted!');
+    } 
+    return Math.max(...arr)
+}
+console.log(findMax([5, 7, 9, 2, 12, 4, 3, 21]));
+
--- 11_day_practise.js
+++ 11_day_practise.js
@@ -1,42 +1,16 @@
 
 const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-// for (let i=0; i<weekDays.length; i++) {
-    //     const oneDay = weekDays[i];
-    //     console.log(oneDay === 'Saturday' || oneDay === 'Sunday' ? oneDay : 'Not a weekend :(' );
-    // }
     
-// for (let i=0; i<weekDays.length; i++) {
-//     const oneDay = weekDays[i];
-//     if(oneDay === 'Saturday' || oneDay === 'Sunday') {
-//         console.log(oneDay)
-//     }
-// }
-
-// console.log('bo')
-    
-// for (let i=0; i<weekDays.length; i++) {
-//     const oneDay = weekDays[i];
-//     if(oneDay !== 'Saturday' && oneDay !== 'Sunday') {
-//         console.log(oneDay)
-//     }
-// }
+for (let i=0; i<weekDays.length; i++) {
+    const oneDay = weekDays[i];
+    if(oneDay !== 'Saturday' && oneDay !== 'Sunday') {
+        console.log(oneDay)
+    }
+}
 
 let boHobbies = ['running', 'korean dance', 'yoga', 'animals', 'nature', 'hiking'];
 let tinaHobbies = ['yoga', 'running', 'writing', 'fitness', 'meditation', 'listening to music'];
-// console.log(bo)
 
-// bo.push('writing')
-// console.log(bo)
-
-// let commonHobbies = [];
-// for (let i = 0; i < boHobbies.length; i++) {
-//     for (let j = 0; j < tinaHobbies.length; j++) {
-//         if (boHobbies[i] === tinaHobbies[j]) {
-//             commonHobbies.push(tinaHobbies[j]);
-//         }
-//     }
-// }
 let commonHobbies = [];
 for (let i = 0; i < boHobbies.length; i++) {
     for (let j = 0; j < tinaHobbies.length; j++) {
